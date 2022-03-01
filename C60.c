42d7441d1d1af4284795128c2dd3d325c6d58972
Lukáš Tesař <lukastesar03@gmail.com>
2022-03-01 08:14:34 +0100

    Sequential search

---
+++ C60.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main() {
+    
+    int v, n;
+    int found = 0;
+    int i;
+
+    printf("Zadejte vzor: ");
+    scanf("%i", &v);
+
+    printf("Zadejte pocet cisel: ");
+    scanf("%i", &n);
+    int a[n];
+
+    printf("Zadejte radu cisel: \n");
+    for(i = 0; i < n; i++) {
+        scanf("%i", &a[i]);
+    }
+
+    printf("\n");
+
+    for(i = 0; i < n; i++) {
+        if(a[i] == v) {
+            found = i;
+            break;
+        }
+    }
+
+    if(found != 0) {
+        printf("Prvek nalezen na %i. pozici (index %i).", found + 1, found);
+    } else {
+        printf("Prvek nenalezen.");
+    }    
+    printf("\n\n");
+    system("PAUSE");
+}
