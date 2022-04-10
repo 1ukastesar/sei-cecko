5ae8f39c527150201939f0a6272746dbf038b34e
Lukáš Tesař <lukastesar03@gmail.com>
2022-04-10 22:40:25 +0200

    Big changes in C57: Selection sort
    One standardized way through all the sorting algorithms

--- C57.c
+++ C57.c
@@ -2,45 +2,35 @@
 #include<stdlib.h>
 
 #include "pause.c"
+#include "list.c"
 
-#define count(array) (sizeof(array) / sizeof((array)[0]))
-
-int main() {
-
-    int n;
-
-    printf("Zadejte pocet cisel: ");
-    scanf("%i", &n);
-
-    int list[n];
-
-    printf("Zadejte cisla: \n");
-    for (int i = 0; i < n; i++)
-    {
-        scanf("%i", &list[i]);
-    }
+void selectionSort(int * list, int length) {
     
+    int i, j;
     int temp, min;
-
-    for (int i = 0; i < (count(list) - 1); i++)
-    {
-        min = (count(list) - 1);
-        
-        for (int j = i; j < (count(list) - 1); j++)
+    for (i = 0; i < (length - 1); i++) {
+        min = (length - 1);
+        // hledani minima
+        for (j = i; j < (length - 1); j++)
         {
             if(list[min] > list[j])
                 min = j;
         }
-
+        // prohozeni prvku
         temp = list[min];
         list[min] = list[i];
         list[i] = temp;
     }
+}
 
-    for (int i = 0; i < n; i++) 
-    {
-        printf("%i ", list[i]);
-    }
+int main() {
+
+    int n = askHowMany();
+    int list[n];
+    
+    loadNumbers(n, list);
+    selectionSort(list, count(list));
+    printList(n, list);
 
     pause();
 }
